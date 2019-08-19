0b85834ff1b8ece07446f8f038c613d596d063b2
Luke McCulloch <tlukemcculloch@gmail.com>
2019-08-19 15:49:21 -0600

    comments during graphical ~boot up~

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,16 @@ int main( int argc, char** argv )
       return 1;
    }
 
+   std::cout << "--------- ---------" << std::endl;
+   std::cout << "Creating viewer" << std::endl;
    Viewer viewer;
+
+   std::cout << "--------- ---------" << std::endl;
+   std::cout << "read tri soup?" << std::endl;
    viewer.mesh.read( argv[1] );
+
+   std::cout << "--------- ---------" << std::endl;
+   std::cout << "viewer init" << std::endl;
    viewer.init();
 
    return 0;
